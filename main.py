27cc8a879f8bf2ffb7da43430da4dbb0e7d1190a
Vivek Mistry <mistry.vivek03@gmail.com>
2021-11-22 15:04:12 +0000

    Setting up the class and controllng path to search

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 #To focus on the control of audio playback.
 class player:
     def __init__(self):
@@ -15,15 +17,34 @@ class player:
 #To focus on traversing file directories in order to find audio files.
 class finder:
     def __init__(self):
-        pass
+        """Sets the default starting directory as the current folder.
+           Creates a list to save items that have been found to.
+        """
+        self._working_dir = os.getcwd()
+        self._discovered_items = []
 
-    @property
     def current_dir(self):
-        pass
+        """Tells the user which directory will be searched
 
-    @current_dir.setter
-    def current_dir(self):
-        pass
-        
+        Returns:
+            String: Path within the file system.
+        """
+        return self._working_dir
+
+    def set_working_dir(self, path):
+        """Changes the directory that will be searched
+
+        Args:
+            path (string): Path within the file system.
+
+        Raises:
+            TypeError: A blank string would not be a valid path.
+        """
+        if path == None:
+            raise TypeError("A valid path must be entered.")
+        else:
+            self._working_dir = path
+
     def search(self):
         pass
+
