8154566b9d19e88afc72a492dcaf94d3c10f3b7a
Vivek Mistry <mistry.vivek03@gmail.com>
2021-11-22 14:46:58 +0000

    Outline for second set of methods for searching

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#To focus on the control of audio playback.
 class player:
     def __init__(self):
         pass
@@ -11,3 +12,18 @@ class player:
     def stop(self):
         pass
 
+#To focus on traversing file directories in order to find audio files.
+class finder:
+    def __init__(self):
+        pass
+
+    @property
+    def current_dir(self):
+        pass
+
+    @current_dir.setter
+    def current_dir(self):
+        pass
+        
+    def search(self):
+        pass
